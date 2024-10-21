60ef964e9dbbf7e6a741bb37de5d29e152049065
Dens Enrijs Lakučs <denslakucs@inbox.lv>
2024-10-21 18:59:17 +0300

    Fixed function buttons, input and result area and floating point

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -19,15 +19,16 @@ import org.mozilla.javascript.Scriptable;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+import java.math.BigDecimal;
 
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView result, calculation;
     Button btn_0, btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9;
     Button btn_plus, btn_minus, btn_mult, btn_div, btn_dot, btn_equal;
     Button btn_msave, btn_mread, btn_mclear;
     Button btn_clear;
+    boolean isNumberPressed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,33 +40,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         result = findViewById(R.id.result);
         calculation = findViewById(R.id.calculation);
-        assignId(btn_0, R.id.btn_0);
-        assignId(btn_1, R.id.btn_1);
-        assignId(btn_2, R.id.btn_2);
-        assignId(btn_3, R.id.btn_3);
-        assignId(btn_4, R.id.btn_4);
-        assignId(btn_5, R.id.btn_5);
-        assignId(btn_6, R.id.btn_6);
-        assignId(btn_7, R.id.btn_7);
-        assignId(btn_8, R.id.btn_8);
-        assignId(btn_9, R.id.btn_9);
-        assignId(btn_plus, R.id.btn_plus);
-        assignId(btn_minus, R.id.btn_minus);
-        assignId(btn_mult, R.id.btn_mult);
-        assignId(btn_div, R.id.btn_div);
-        assignId(btn_dot, R.id.btn_dot);
-        assignId(btn_equal, R.id.btn_equal);
-        assignId(btn_msave, R.id.btn_msave);
-        assignId(btn_mread, R.id.btn_mread);
-        assignId(btn_mclear, R.id.btn_mclear);
-        assignId(btn_clear, R.id.btn_clear);
+
+        // Initialize buttons
+        initButtons();
+
+        // Disable 0 and operation buttons until a number is pressed
+        disableButtons(btn_0, btn_plus, btn_minus, btn_mult, btn_div);
     }
 
-    void assignId(Button btn, int id){
-        btn = findViewById(id);
-        btn.setOnClickListener(this);
+    private void initButtons() {
+        btn_0 = findViewById(R.id.btn_0);
+        btn_1 = findViewById(R.id.btn_1);
+        btn_2 = findViewById(R.id.btn_2);
+        btn_3 = findViewById(R.id.btn_3);
+        btn_4 = findViewById(R.id.btn_4);
+        btn_5 = findViewById(R.id.btn_5);
+        btn_6 = findViewById(R.id.btn_6);
+        btn_7 = findViewById(R.id.btn_7);
+        btn_8 = findViewById(R.id.btn_8);
+        btn_9 = findViewById(R.id.btn_9);
+        btn_plus = findViewById(R.id.btn_plus);
+        btn_minus = findViewById(R.id.btn_minus);
+        btn_mult = findViewById(R.id.btn_mult);
+        btn_div = findViewById(R.id.btn_div);
+        btn_dot = findViewById(R.id.btn_dot);
+        btn_equal = findViewById(R.id.btn_equal);
+        btn_clear = findViewById(R.id.btn_clear);
+        btn_msave = findViewById(R.id.btn_msave);
+        btn_mread = findViewById(R.id.btn_mread);
+        btn_mclear = findViewById(R.id.btn_mclear);
+
+        // Set click listeners
+        btn_0.setOnClickListener(this);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+        btn_7.setOnClickListener(this);
+        btn_8.setOnClickListener(this);
+        btn_9.setOnClickListener(this);
+        btn_plus.setOnClickListener(this);
+        btn_minus.setOnClickListener(this);
+        btn_mult.setOnClickListener(this);
+        btn_div.setOnClickListener(this);
+        btn_dot.setOnClickListener(this);
+        btn_equal.setOnClickListener(this);
+        btn_clear.setOnClickListener(this);
+        btn_msave.setOnClickListener(this);
+        btn_mread.setOnClickListener(this);
+        btn_mclear.setOnClickListener(this);
+    }
+
+    private void disableButtons(Button... buttons) {
+        for (Button button : buttons) {
+            button.setEnabled(false);
+        }
+    }
+
+    private void enableButtons(Button... buttons) {
+        for (Button button : buttons) {
+            button.setEnabled(true);
+        }
     }
 
     @Override
@@ -73,55 +113,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btn = (Button) view;
         String btnText = btn.getText().toString();
         String calculationData = calculation.getText().toString();
-        if(btnText.equals("C")){
+
+        if (btnText.equals("C")) {
             calculation.setText("");
             result.setText("0");
+            disableButtons(btn_0, btn_plus, btn_minus, btn_mult, btn_div);
+            isNumberPressed = false;
             return;
         }
-        if(btnText.equals("MS")){
+
+        if (btnText.equals("MS")) {
             saveInMemory();
             return;
         }
-        if(btnText.equals("MR")){
+
+        if (btnText.equals("MR")) {
             readFromMemory(calculationData);
             return;
         }
-        if(btnText.equals("MC")){
+
+        if (btnText.equals("MC")) {
             clearFromMemory(calculationData);
             return;
         }
-        if (btnText.equals("=")){
+
+        if (btnText.equals("=")) {
             calculation.setText("");
-            result.getText();
             return;
-        }else{
-            calculationData = calculationData + btnText;
+        } else {
+            if (!isNumberPressed && (btnText.equals("0") || btnText.matches("[+\\-*/]"))) {
+                return; // Prevent pressing 0 or math operations first
+            }
+            calculationData += btnText;
+            calculation.setText(calculationData);
         }
-        calculation.setText(calculationData);
 
         String calculationResult = getResult(calculationData);
-
         if (!calculationResult.equals("Err")) {
             result.setText(calculationResult);
         }
+        isNumberPressed = true;
+        enableButtons(btn_0, btn_plus, btn_minus, btn_mult, btn_div);
     }
 
-    String getResult(String data){
-        try{
-            Context context = Context.enter();
-            context.setOptimizationLevel(-1);
-            Scriptable scriptable = context.initStandardObjects();
-            String calculationResult = context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
-            if(calculationResult.endsWith(".0")){
-                calculationResult = calculationResult.replace(".0", "");
-            }
+    String getResult(String data) {
+        try {
+            Context rhino = Context.enter();
+            rhino.setOptimizationLevel(-1);
+            Scriptable scope = rhino.initStandardObjects();
+            String calculationResult = rhino.evaluateString(scope, data, "JavaScript", 1, null).toString();
+
+            // Limit the decimal places to avoid floating-point inaccuracies
+            BigDecimal resultValue = new BigDecimal(calculationResult);
+            resultValue = resultValue.setScale(10, BigDecimal.ROUND_HALF_UP);  // Up to 10 decimal places
+
+            calculationResult = resultValue.stripTrailingZeros().toPlainString();
             return calculationResult;
-        }catch (Exception e){
+        } catch (Exception e) {
             return "Err";
+        } finally {
+            Context.exit();
         }
     }
 
-    private void saveInMemory(){
+    // Memory functions
+    private void saveInMemory() {
         String result = this.result.getText().toString();
         DataStoreManager.getInstance(this).getDataStore()
                 .updateDataAsync(prefsIn -> {
@@ -135,7 +191,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @SuppressLint({"CheckResult", "SetTextI18n"})
-    private void readFromMemory(String calculationData){
+    private void readFromMemory(String calculationData) {
         DataStoreManager.getInstance(this).getDataStore()
                 .data()
                 .subscribeOn(Schedulers.io())
@@ -143,32 +199,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .subscribe(prefs -> {
                     String savedMemory = prefs.get(DataStoreManager.MEMORY_KEY);
                     if (savedMemory != null) {
+                        // Update the calculation and result TextViews
                         calculation.setText(calculationData + savedMemory);
                         result.setText(getResult(calculationData + savedMemory));
+
+                        // Enable operation buttons and 0
+                        enableButtons(btn_0, btn_plus, btn_minus, btn_mult, btn_div);
+
+                        // Set the flag that a number has been pressed
+                        isNumberPressed = true;
                     }
                 });
     }
 
+
     @SuppressLint("CheckResult")
-    private void clearFromMemory(String calculationData){
+    private void clearFromMemory(String calculationData) {
         DataStoreManager.getInstance(this).getDataStore()
-                .updateDataAsync(prefsIn ->{
+                .updateDataAsync(prefsIn -> {
                     MutablePreferences mutablePreferences = prefsIn.toMutablePreferences();
                     mutablePreferences.remove(DataStoreManager.MEMORY_KEY);
                     return Single.just(mutablePreferences);
                 })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        prefs ->{
-                            if(!calculationData.isEmpty()){
-                                calculation.setText(calculationData);
-                                result.setText(getResult(calculationData));
-                            }else{
-                                calculation.setText("");
-                                result.setText("0");
-                            }
-                        }
-                );
+                .subscribe(prefs -> {
+                    if (!calculationData.isEmpty()) {
+                        calculation.setText(calculationData);
+                        result.setText(getResult(calculationData));
+                    } else {
+                        calculation.setText("");
+                        result.setText("0");
+                    }
+                });
     }
 }
